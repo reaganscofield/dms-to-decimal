ddec70d35c38772a8bf971da5c91b9daf15b2aab
reaganscofield <reaganscofieldkayembe@outlook.com>
2021-05-21 10:17:34 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # dms-to-decimal
 A JavaScript library for converting degrees, minutes, and seconds to decimal
+
+# Installation
+
+```
+    npm install dms-to-decimal
+```
+
+or 
+
+```
+    yarn add dms-to-decimal
+```
+
+# Example
+
+```
+    import dms2Decimal from 'dms-to-decimal';
+
+    const degree = 46;
+    const minutes = 59;
+    const seconds = 5;
+    const direction = 'N';
+
+    const converDms2Decimal = dms2Decimal(degree, minutes, seconds, direction);
+
+    console.log(converDms2Decimal); // Output: 46.984722222222224
+```
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function DMS2Decimal(degrees = 0, minutes = 0, seconds = 0, direction = 'N') {
     const directions = ['N', 'S', 'E', 'W'];
-    if(!directions.includes(direction.toLocaleUpperCase())) return false;
-    if(!Number(minutes) || minutes < 0 || minutes > 59) return false;
-    if(!Number(seconds) || seconds < 0 || seconds > 59) return false;
-    if(!Number(degrees) || degrees < 0 || degrees > 180) return false;
+    if(!directions.includes(direction.toUpperCase())) return 0;
+    if(!Number(minutes) || minutes < 0 || minutes > 59) return 0;
+    if(!Number(seconds) || seconds < 0 || seconds > 59) return  0;
+    if(!Number(degrees) || degrees < 0 || degrees > 180) return 0;
     
     let decimal = degrees + (minutes / 60) + (seconds / 3600);
-    if (direction.toLocaleUpperCase() === 'S' || direction.toLocaleUpperCase() === 'W') decimal *= -1;
+    if (direction.toUpperCase() === 'S' || direction.toUpperCase() === 'W') decimal *= -1;
     return decimal;
 }
 
